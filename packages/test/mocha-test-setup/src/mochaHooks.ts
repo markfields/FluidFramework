8dde5432cc68f51c4c43fa4bc8af09be936f0b22
Mark Fields <markfields@users.noreply.github.com>
2020-09-28 13:17:46 -0700

    mochaHooks fixes

--- packages/test/mocha-test-setup/src/mochaHooks.ts
+++ packages/test/mocha-test-setup/src/mochaHooks.ts
@@ -5,7 +5,13 @@
 
 /* eslint-disable @typescript-eslint/unbound-method */
 const log = console.log;
-const error = console.log;
+const error = console.error;
+if (process.env.FLUID_TEST_VERBOSE === undefined) {
+    console.log(`
+NOTE: console.log and console.error are suppressed.
+      set FLUID_TEST_VERBOSE env var to 1 to enable,
+      or use :verbose version of test script if present in package.json`);
+}
 export const mochaHooks = {
     beforeEach() {
         if (process.env.FLUID_TEST_VERBOSE === undefined) {
